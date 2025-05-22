4d3c783dfc3426e74d71bdc9ba21eb76c3d3d534
Samuel <30960735+slownie@users.noreply.github.com>
2025-05-22 12:02:21 -0400

    changes

--- src/views/test/PracticeTestView.vue
+++ src/views/test/PracticeTestView.vue
@@ -6,30 +6,61 @@ import testData from "@/data/jlptTests.json";
 const props = defineProps({ id: String });
 const propArray = props.id?.split("-");
 
-const questions = () => {
-  return testData[5 - parseInt(propArray[0].charAt(1))];
-};
+// Setup Questions Array
+let questions;
+if (propArray[1] == "jlpt") {
+  questions = testData[5 - parseInt(propArray[0].charAt(1))];
+} else {
+  questions = testData[5 - parseInt(propArray[0].charAt(1))][propArray[1]];
+}
+
+for (const [key, value] of Object.entries(questions)) {
+  console.log(key);
+}
+
+
+
 // Test Setup
 const quizCompleted = ref(false);
 const currentQuestion = ref(0);
 
-const score = computed(() => {
-  let value = 0;
+const currentAnswer = ref("");
 
-  // Setting the score
-
-  return value;
-});
+const score = ref(0);
 
 // Get the current question via index
-const getCurrentQuestion = computed(() => {});
+const getCurrentQuestion = computed(() => {
+  let question = questions.questions[0];
+  return question
+});
+
+// Increment to the next question
+const getNextQuestion = () => {
+  if (currentQuestion.value < questions.length - 1) {
+    currentQuestion.value++;
+    return;
+  }
+  quizCompleted.value = true;
+}
 </script>
 
 <template>
   <main>
     <h1>Practice Test View</h1>
     {{ props.id }}
-    <section class="quiz" v-if="!quizCompleted"></section>
+    <p>Score: {{ score }} / </p>
+    <section class="quiz" v-if="!quizCompleted">
+      <!--Question Sentence-->
+      <p v-html="getCurrentQuestion.sentence"></p>
+
+      <!--Options-->
+      <div class="options" v-for="option in getCurrentQuestion?.answers">
+        <input type="radio" v-model="currentAnswer" :value="option" :id="option">
+        <label :for="option">{{ option }}</label>
+      </div>
+      
+      <button :disabled="currentAnswer===''">Submit</button>
+    </section>
     <section v-else>
       <h2>You have finished the test!</h2>
     </section>
