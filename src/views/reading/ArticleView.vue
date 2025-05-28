f29fe2f865d69f58925ce4272f0e1a631dbb1890
Samuel <30960735+slownie@users.noreply.github.com>
2025-05-28 09:41:03 -0400

    an example for reading articles

--- src/views/reading/ArticleView.vue
+++ src/views/reading/ArticleView.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import articleData from "@/data/articles.json";
 
+const showAnswer = ref(false);
 const props = defineProps({ id: String });
 
 const currentArticle = computed(() => {
@@ -12,5 +13,26 @@ const currentArticle = computed(() => {
 <template>
   <main>
     <h1>{{ currentArticle.title }}</h1>
+    元記事:
+    <a :href="currentArticle?.source" target="_blank">{{
+      currentArticle?.source
+    }}</a>
+    <h3>文法</h3>
+    <ul v-for="grammarPoint in currentArticle?.grammarPointsUsed">
+      <li>{{ grammarPoint }}</li>
+    </ul>
+    <hr />
+    <p v-html="currentArticle.article_html"></p>
+    <hr />
+    <h2>質問</h2>
+    <div v-for="question in currentArticle.questions">
+      {{ question }}
+    </div>
+    <button @click="showAnswer = !showAnswer">Show Answers</button>
+    <div v-if="showAnswer">
+      <div v-for="answer in currentArticle?.answers">
+        {{ answer }}
+      </div>
+    </div>
   </main>
 </template>
